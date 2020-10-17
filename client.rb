1b9ac6e4cabf5433b6e840d1ee7653d03e3f59b7
ishantkumar <mr.ishantkumar@gmail.com>
2020-10-17 20:01:56 +0530

    Create client.rb

---
+++ client.rb
@@ -0,0 +1,7 @@
+chef_server_url  'https://api.chef.io/organizations/<orgname>'
+validation_client_name '<orgname>-validator'
+validation_key '/etc/chef/validator.pem'
+client_key '/etc/chef/client.pem'
+
+
+## Reference https://docs.chef.io/config_rb_client/
