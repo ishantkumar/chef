9c15e3a5d656d8130da44c3778710e492483400f
ishantkumar <mr.ishantkumar@gmail.com>
2020-10-20 18:51:23 +0530

    Update default.rb

--- cookbooks/httpd/recipes/default.rb
+++ cookbooks/httpd/recipes/default.rb
@@ -15,3 +15,10 @@ cookbook_file '/var/www/html/index.html' do
 source 'index.html'
 mode '0644'
 end
+
+template '/var/www/html/index4.html' do
+  source 'index4.erb'
+  variables(
+    :myname => node['httpd']['myname']
+  )
+end
